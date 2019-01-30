9af760bc9dda5285c0f955fc93b455ccc3ee3a7b
Alex <alex.harry1@gmail.com>
2019-01-29 21:44:42 -0700

    removed old files

--- PsychiatricGuideApp/app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'app/login.html', {'title': 'Login'}) # request, html path, title
