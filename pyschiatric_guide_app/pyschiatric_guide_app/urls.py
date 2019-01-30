421169115dd168ad76f29c7132391912ec10cb58
Alex <alex.harry1@gmail.com>
2019-01-29 22:33:23 -0700

    Added non functional username/password box

--- pyschiatric_guide_app/pyschiatric_guide_app/urls.py
+++ pyschiatric_guide_app/pyschiatric_guide_app/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as log_view # views for login/logout provided by django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('application.urls')),  # '' Sets home page
+    # path('', include('application.urls')),  # '' Sets home page
     path('application/', include('application.urls')),
-
+    path('', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"), # Home page of entire site
+    path('login/', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"), # login page at /login
+    path('logout', log_view.LogoutView.as_view(template_name='application/global-html.html'), name="logout-view") # Logout page at
 ]
