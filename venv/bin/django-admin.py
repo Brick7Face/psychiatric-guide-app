2398a8de4d31e8f14c6fdfcf9d559f20c5cba5ba
Alex <alex.harry1@gmail.com>
2019-01-29 17:44:59 -0700

    Added git ignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/alex/Documents/Montana State/2019/Spring/ESOF_432/PsychiatricGuideApp/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
