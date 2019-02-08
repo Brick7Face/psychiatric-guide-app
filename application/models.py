e1203c619511820858717b9028e8655ce55a6ea1
Nate Tranel <njtranel@gmail.com>
2019-02-08 11:45:34 -0700

    starts making models...

--- application/models.py
+++ application/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Steps(models.Model):
+	
+	name = models.TextField(help_text='Enter step title')
+	description = models.TextField(help_text='Enter step description')
+    
+    # Metadata
+    class Meta: 
+        ordering = ['title']
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+    
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.name
