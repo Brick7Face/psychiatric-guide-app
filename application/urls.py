93767dc397276ba21b4b049f83ba80938d575900
Bryan Plant <bryanplant@gmail.com>
2019-02-07 22:08:23 -0700

    basic survey template html

--- application/urls.py
+++ application/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as log_view # views for login/logout provided by django
 
+from application import views
 
 urlpatterns = [
     # path('', views.login, name='application-login'), # '' Directs to main /application page
-    path('', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"), # Home page of entire site
-    path('login/', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"), # login page at /login
-    path('logout', log_view.LogoutView.as_view(template_name='application/global-html.html'), name="logout-view") # Logout page at
 
+    # Home page of entire site
+    path('', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"),
+    # login page at /login
+    path('login/', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"),
+    # Logout page at
+    path('logout', log_view.LogoutView.as_view(template_name='application/global-html.html'), name="logout-view"),
+    # survey page at survey/
+    path('survey/', views.survey, name="survey-view")
 ]
--- application/views.py
+++ application/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'application/login.html', {'title': 'Login'})  # Renders login.html
+
+
+def survey(request):
+    questions = [
+        "Little interest or pleasure in doing things?",
+        "Feeling down, depressed or hopeless?",
+        "Trouble falling or staying asleep, or sleeping too much?",
+        "Feeling tired or having little energy?",
+        "Poor appetite or overeating?",
+        "Feeling bad about yourself — or that you are a failure or have let yourself or your family down?",
+        "Trouble concentrating on things, such as reading the newspaper or watching television?",
+        "Moving or speaking so slowly that other people could have noticed? Or so fidgety or restless that you have been moving a lot more than usual?",
+        "Thoughts that you would be better off dead, or thoughts of hurting yourself in some way?",
+        "How difficult have these problems made it for you to do your work, take care of things at home, or get along with other people?"
+    ]
+    return render(request, 'application/survey.html', {'questions': questions})
