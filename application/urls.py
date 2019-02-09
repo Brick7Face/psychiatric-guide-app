26426cae3078edbbeff8b43a558a13ba3695316c
Alex <alex.harry1@gmail.com>
2019-02-09 14:01:27 -0700

    Users can be created/logged in on the website now without going through admin page

--- application/urls.py
+++ application/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as log_view # views for login/logout provided by django
-
-
+from . import views
 urlpatterns = [
     # path('', views.login, name='application-login'), # '' Directs to main /application page
     path('', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"), # Home page of entire site
-    path('login/', log_view.LoginView.as_view(template_name='application/login.html'), name="login-view"), # login page at /login
-    path('logout', log_view.LogoutView.as_view(template_name='application/global-html.html'), name="logout-view") # Logout page at
+    path('login/', log_view.LoginView.as_view(template_name = 'application/login.html'), name='login-view'), # login page at /login
+    path('logout/', log_view.LogoutView.as_view(template_name='application/login.html'), name="logout-view"), # Logout page at
+    path('create_user/', views.create_user, name='create-new-user'),# Path for new user registration
+    path('backend_home/', log_view.LogoutView.as_view(template_name='application/backend-home.html'), name="backend-home"), # Home page for logged in user
+
+    # Logout page at
 
 ]
--- application/views.py
+++ application/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django import forms
 
 
 # Create your views here.
 
+
 def login(request):
-    return render(request, 'application/login.html', {'title': 'Login'})  # Renders login.html
+    return render(request, 'login-view', {'title': 'Login'})  # Renders login.html
+
+
+def new_user(request):
+    return render(request, 'application/new-user.html', {'title': 'User Registration'})  # Renders new-user.html
+
+
+# Create user code helped from Youtube series by Cory Schafer: https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p
+def create_user(request):  # Renders user creation
+    if request.method == 'POST':
+        new_user_form = CreateUser(request.POST)  # Form for creating a new user
+        if new_user_form.is_valid():  # Enters if form is valid
+            new_user_form.save()  # Saves form to database
+            username = new_user_form.cleaned_data.get('username')
+            first_name = new_user_form.cleaned_data.get('first_name')
+            last_name = new_user_form.cleaned_data.get('last_name')
+            messages.success(request, 'Account created.')  # Message if user created succesfully
+            return redirect('application/backend-home.html')
+
+    else:
+        new_user_form = CreateUser()  # Resets form with error message if attempt not valid
+    return render(request, 'application/new-user.html', {'new_user_form': new_user_form})
+
+
+class CreateUser(UserCreationForm):  # Class for the user generation form
+    username = forms.CharField()  # gets username
+    first_name = forms.CharField()  # gets first name
+    last_name = forms.CharField()  # Gets last name.
+    is_super_user = forms.CheckboxInput()  # check
+    is_staff = forms.CheckboxInput()
+
+    class Meta:
+        model = User  # Form is of User Model.
+        fields = ['username', 'first_name', 'last_name', 'is_staff', 'is_superuser',
+                  'password1']  # Fields to be displayed of the form on the site.
