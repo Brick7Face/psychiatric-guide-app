9e1b39cbdb9b67c9484c7eabdb36f2c84fb729a9
Bryan Plant <bryanplant@gmail.com>
2019-02-11 08:53:15 -0700

    hide next button until a choice is selected

--- application/static/application/survey.js
+++ application/static/application/survey.js
@@ -1,26 +1,29 @@
-var slideIndex = 1;
+var questionIndex = 1;
+var questions = [];
 window.onload = function () {
-    showQuestion(slideIndex);
+    showQuestion(questionIndex);
 };
 
 // Next/previous controls
 function updateQuestion(n) {
-    showQuestion(slideIndex += n);
+    showQuestion(questionIndex += n);
 }
 
 function showQuestion(n) {
     var i;
-    var questions = document.getElementsByClassName("question");
+    questions = document.getElementsByClassName("question");
     if (n > questions.length) {
-        slideIndex = 1
+        questionIndex = 1
     }
     if (n < 1) {
-        slideIndex = questions.length
+        questionIndex = questions.length
     }
     for (i = 0; i < questions.length; i++) {
         questions[i].style.display = "none";
-        questions[i].className = questions[i].className.replace(" active", "");
     }
-    questions[slideIndex-1].style.display = "block";
-    questions[slideIndex-1].className += " active"
+    questions[questionIndex-1].style.display = "block";
+}
+
+function showNext() {
+    questions[questionIndex-1].querySelector("#next-button").style.display = "block";
 }
