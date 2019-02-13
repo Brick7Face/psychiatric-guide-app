41f7ce9c5ff62c3037fe6149afd9b471d6170392
Madison Fichtner <madisontfichtner@msn.com>
2019-02-13 09:03:36 -0700

    added missing static variable in settings.py

--- psychiatric_guide_app/settings.py
+++ psychiatric_guide_app/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = ( os.path.join('static'), )
 LOGIN_REDIRECT_URL = 'backend-home'  # Sets redirect after login
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
