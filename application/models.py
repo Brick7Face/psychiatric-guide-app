31b99f67a865798ed26a45625775f3561e80f373
Brick7Face <njtranel@gmail.com>
2019-02-13 09:52:08 -0700

    stuff works currently, nearly done

--- application/models.py
+++ application/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # defines model for disorder treatment type and corresponding steps
 class Treatment(models.Model):
     # Fields
-    name = models.TextField(help_text='Enter disorder treatment type title')
+    name = models.CharField(help_text='Enter disorder treatment type title', max_length=25)
     
     # Metadata
     class Meta: 
@@ -23,7 +23,7 @@ class Treatment(models.Model):
 # defines model for a single step (or box) from prescribing guide flowchart
 class Step(models.Model):
     # Fields
-    name = models.TextField(help_text='Enter step title')
+    name = models.CharField(help_text='Enter step title', max_length=25)
     description = models.TextField(help_text='Enter step description')
     treatment = models.ForeignKey(Treatment, on_delete=models.SET_DEFAULT, default='')
     
@@ -43,8 +43,8 @@ class Step(models.Model):
 # defines model for a medication used by prescribers
 class Medication(models.Model):
     # Fields
-    name = models.TextField(help_text='Enter medication name')
-    category = models.TextField(help_text='Enter medication type')
+    name = models.CharField(help_text='Enter medication name', max_length=25)
+    category = models.CharField(help_text='Enter medication type', max_length=50)
     initial_Dose = models.FloatField(help_text='Enter medication initial dose in mg')
     maximum_Dose = models.FloatField(help_text='Enter medication maximum dose in mg')
     titration = models.TextField(help_text='Enter medication titration information')
@@ -86,12 +86,12 @@ class Prescriber(models.Model):
 # defines model for a patient in treatment process
 class Patient(models.Model):
     # Fields
-    first_Name = models.TextField(help_text='Enter patient first name and middle initial')
-    last_Name = models.TextField(help_text='Enter patient last name')
+    first_Name = models.CharField(help_text='Enter patient first name and middle initial', max_length=30)
+    last_Name = models.CharField(help_text='Enter patient last name', max_length=50)
     DOB = models.DateField(help_text='Enter patient date of birth')
-    address = models.TextField(help_text='Enter patient current address')
-    email = models.EmailField(help_text='Enter patient current email')
-    phone = models.TextField(help_text='Enter patient current phone number')
+    address = models.CharField(help_text='Enter patient current address', max_length=150)
+    email = models.EmailField(help_text='Enter patient current email', max_length=50)
+    phone = models.CharField(help_text='Enter patient current phone number', max_length=20)
     prescriber = models.ForeignKey(Prescriber, null=True, on_delete=models.SET_NULL)
     current_Step = models.ForeignKey(Step, null=True, on_delete=models.SET_NULL)
     # list of PHQ-9 scores
@@ -112,5 +112,5 @@ class Patient(models.Model):
     
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.name
+        return self.last_Name
 
