60d8e8f995282ad9eacdb3af81b8c6cc7d620aa8
Bryan Plant <bryanplant@gmail.com>
2019-02-22 11:45:08 -0700

    use sqlite3 db when running locally

--- psychiatric_guide_app/settings.py
+++ psychiatric_guide_app/settings.py
@@ -91,20 +91,10 @@ if os.getenv('GAE_APPLICATION', None):
         }
     }
 else:
-    # Running locally so connect to either a local MySQL instance or connect to
-    # Cloud SQL via the proxy. To start the proxy via command line:
-    #
-    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-    #
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-            'NAME': 'guide',
-            'USER': 'bryanplant',
-            'PASSWORD': '',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 # [END db_setup]
