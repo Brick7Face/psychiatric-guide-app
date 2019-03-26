4ddcadcf3423eb5f0464140b854eb98e3fcff634
Brick7Face <njtranel@gmail.com>
2019-03-26 17:29:29 -0600

    adds tests for apps.py, refactors existing tests to have one assert per testcase

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from application.models import Step, Treatment, Medication, Prescriber, Patient, Phq9
 from application.questionnaire_evaluations import PHQ9
+from application.apps import ApplicationConfig
 from django.urls import reverse
+from django.apps import apps
 
 class PHQ9TestCase(TestCase):
     phq9 = None
@@ -9,78 +11,109 @@ class PHQ9TestCase(TestCase):
     def setUp(self):
         self.phq9 = PHQ9()
 
-    def test_diagnosis_one(self):
+    def test_diagnosis_one_one(self):
         a1 = {1: 2, 2: 0}
-        a2 = {1: 3, 2: 0}
-        a3 = {1: 0, 2: 2}
-        a4 = {1: 0, 2: 3}
-        a5 = {1: 0, 2: 0}
-        a6 = {1: 2, 2: 3}
-        a7 = None
         self.assertTrue(self.phq9.diagnosis_one(a1))
+		
+    def test_diagnosis_one_two(self):
+        a2 = {1: 3, 2: 0}
         self.assertTrue(self.phq9.diagnosis_one(a2))
+	
+    def test_diagnosis_one_three(self):
+        a3 = {1: 0, 2: 2}
         self.assertTrue(self.phq9.diagnosis_one(a3))
+		
+    def test_diagnosis_one_four(self):
+        a4 = {1: 0, 2: 3}
         self.assertTrue(self.phq9.diagnosis_one(a4))
+	
+    def test_diagnosis_one_five(self):
+        a5 = {1: 0, 2: 0}
         self.assertFalse(self.phq9.diagnosis_one(a5))
+		
+    def test_diagnosis_one_six(self):
+        a6 = {1: 2, 2: 3}
         self.assertTrue(self.phq9.diagnosis_one(a6))
+		
+    def test_diagnosis_one_seven(self):
+        a7 = None
         self.assertFalse(self.phq9.diagnosis_one(a7))
 
-    def test_diagnosis_two(self):
+    def test_diagnosis_two_one(self):
         # none
         a1 = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+        self.assertFalse(self.phq9.diagnosis_two(a1))
+		
+    def test_diagnosis_two_two(self):
         # four
         a2 = {1: 2, 2: 2, 3: 2, 4: 2, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+        self.assertFalse(self.phq9.diagnosis_two(a2))
+		
+    def test_diagnosis_two_three(self):
         # five
         a3 = {1: 2, 2: 2, 3: 2, 4: 2, 5: 0, 6: 0, 7: 0, 8: 0, 9: 2}
+        self.assertTrue(self.phq9.diagnosis_two(a3))
+		
+    def test_diagnosis_two_four(self):
         # six
         a4 = {1: 2, 2: 2, 3: 2, 4: 2, 5: 2, 6: 0, 7: 0, 8: 0, 9: 2}
-        self.assertFalse(self.phq9.diagnosis_two(a1))
-        self.assertFalse(self.phq9.diagnosis_two(a2))
-        self.assertTrue(self.phq9.diagnosis_two(a3))
         self.assertTrue(self.phq9.diagnosis_two(a4))
 
-    def test_diagnosis_three(self):
+    def test_diagnosis_three_one(self):
         a1 = {10: 0}
-        a2 = {10: 1}
-        a3 = {10: 2}
         self.assertFalse(self.phq9.diagnosis_three(a1))
+		
+    def test_diagnosis_three_two(self):
+        a2 = {10: 1}
         self.assertFalse(self.phq9.diagnosis_three(a2))
+		
+    def test_diagnosis_three_three(self):
+        a3 = {10: 2}
         self.assertTrue(self.phq9.diagnosis_three(a3))
 
-    def test_diagnosis(self):
+    def test_diagnosis_one(self):
         self.assertTrue(self.phq9.diagnosis(True, True, True))
+		
+    def test_diagnosis_two(self):
         self.assertFalse(self.phq9.diagnosis(True, True, False))
 
-    def test_change_treatment(self):
+    def test_change_treatment_one(self):
         self.assertTrue(self.phq9.change_treatment(True, True))
+		
+    def test_change_treatment_two(self):
         self.assertTrue(self.phq9.change_treatment(True, False))
+		
+    def test_change_treatment_three(self):
         self.assertFalse(self.phq9.change_treatment(False, False))
 
-    def test_suicide_risk(self):
+    def test_suicide_risk_one(self):
         a1 = {9: 0}
-        a2 = {9: 1}
-        a3 = {9: 2}
         self.assertFalse(self.phq9.suicide_risk(a1))
+		
+    def test_suicide_risk_two(self):
+        a2 = {9: 1}
         self.assertTrue(self.phq9.suicide_risk(a2))
+		
+    def test_suicide_risk_three(self):
+        a3 = {9: 2}
         self.assertTrue(self.phq9.suicide_risk(a3))
 
-    def test_treatment_and_monitoring(self):
+    def test_treatment_and_monitoring_one(self):
         a1 = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
-        a2 = {1: 0, 2: 0, 3: 2, 4: 0, 5: 3, 6: 0, 7: 1, 8: 0, 9: 0, 10: 0}
-        a3 = {1: 1, 2: 0, 3: 0, 4: 0, 5: 3, 6: 0, 7: 0, 8: 0, 9: 0, 10: 2}
         self.assertEqual(0, self.phq9.treatment_and_monitoring(a1))
+		
+    def test_treatment_and_monitoring_two(self):
+        a2 = {1: 0, 2: 0, 3: 2, 4: 0, 5: 3, 6: 0, 7: 1, 8: 0, 9: 0, 10: 0}
         self.assertEqual(6, self.phq9.treatment_and_monitoring(a2))
+		
+    def test_treatment_and_monitoring_three(self):
+        a3 = {1: 1, 2: 0, 3: 0, 4: 0, 5: 3, 6: 0, 7: 0, 8: 0, 9: 0, 10: 2}
         self.assertEqual(6, self.phq9.treatment_and_monitoring(a3))
-
 
     def test_convert_results(self):
         a1 = {'1':['2'], '2':['1']}
         a1_expected = {1:2, 2:1}
         self.assertEqual(self.phq9.convert_results(a1), a1_expected)
-
-    def test_phq9_evaluation(self):
-        # TODO: a change in the Severity Score of 5 or more
-        pass
 
 
 class ModelsTestCase(TestCase):
@@ -102,7 +135,6 @@ class ModelsTestCase(TestCase):
         self.assertEqual(str(patient), patient.last_name)
 
 		
-# tests some of the url reversals; testing one covers urls.py, so not every url is tested
 class UrlsTestCase(TestCase):
 	def test_logout_url(self):
 		url = reverse('logout-view')
@@ -143,6 +175,15 @@ class UrlsTestCase(TestCase):
 	def test_bug_report_url(self):
 		url = reverse('bug_report')
 		self.assertEqual(url, '/application/bug_report/')
+	
+class ApplicationConfigTest(TestCase):
+	def test_apps_one(self):
+		self.assertEqual(ApplicationConfig.name, 'application')
+	
+	def test_apps_two(self):
+		self.assertEqual(apps.get_app_config('application').name, 'application')
+
+		
 		
 		
 #
